78a936c1e5ac0f752acd0da7194cd20a00242ba4
jihwankim128 <jihwan1535@nate.com>
2023-05-20 15:11:20 +0900

    Counting Sort

---
+++ 자료구조/solution/10807-김지환.cpp
@@ -0,0 +1,36 @@
+// acmicpc.net/problem/1457
+// author : kimjihwan
+// 2023-05-20 PM 2: 34 
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, v;
+int arr[202];
+
+void input() {
+	cin >> n;
+	for(int i = 0; i<n; i++) {
+		int idx;
+		cin >> idx;
+		arr[idx+100]++;
+	}
+	cin >> v;
+}
+
+void solve() {
+	cout << arr[v+100];
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
+	input();
+	solve();
+	
+	return 0;
+}
+// 2023-05-20 PM 2:35
