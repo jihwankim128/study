3f7f3c362c6d606b7d30195c3a6554e585072b2a
이예은 <102972001+Yenniiii@users.noreply.github.com>
2023-06-30 19:24:36 +0900

    Update sort.py

--- Test/3rdWeek/sort.py
+++ Test/3rdWeek/sort.py
@@ -2,13 +2,14 @@
 
 import sys
 
-n=int(input())
-number=[0]*n
+n=int(sys.stdin.readline())
+number=[0]*10001
 
 for i in range(1,n+1):
     number[int(sys.stdin.readline())]+=1
 
-for i in range(1,n):
+for i in range(1,10001):
     for j in range(number[i]):
         #print(f"number {i}={number[i]}")
-        print(i)
+        sys.stdout.write(f"{i}"+'\n')
+        #다 안 됐는데 stdout바꾸니까 바로 됨
