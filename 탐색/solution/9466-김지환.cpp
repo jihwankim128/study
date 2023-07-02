9d650e3cecf1cc94b95414349d42db974d9c0359
jihwankim128 <jihwan1535@nate.com>
2023-07-03 04:34:16 +0900

    (텀 프로젝트) Fail, Not Solved

---
+++ 탐색/solution/9466-김지환.cpp
@@ -0,0 +1,78 @@
+//acm 9466
+//authored by jihwankim
+// 06-14 AM 03:14
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int stu[100001];
+bool visited[100001];
+bool notCycle[100001];
+bool cycleChk[100001];
+
+void input() {
+	cin >> n;
+	for(int i = 1; i<=n; i++) {
+		cin >> stu[i];
+		notCycle[i] = false;
+		cycleChk[i] = false;
+	}
+}
+
+bool cycle(int num) {
+	queue<int> q;
+	vector<int> v;
+	q.push(num);
+	visited[num] = true;
+	
+	while(!q.empty()) {
+		int x = stu[q.front()];
+		v.push_back(x);
+		q.pop();
+		
+		if(notCycle[x]) return false;
+		
+		if(visited[x]) {
+			if(num == x) {
+				for(int i = 0; i<v.size(); i++) {
+					cycleChk[v[i]] = true;
+				}
+				return true;
+			}
+			continue;
+		}
+		visited[x] = true;
+		q.push(x);
+	}
+	
+	return false;
+}
+
+void solve() {
+	int cnt = 0;
+	for(int i = 1; i<=n; i++) {
+		if(cycleChk[i]) {
+			cnt++;
+			continue;
+		}
+		if(cycle(i)) 	cnt++;
+		else notCycle[i] = true;
+	}
+	cout << n-cnt << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
+	int t;
+	cin >> t;
+	while(t--) {
+		input();
+		solve();
+	}
+	
+	return 0;
+}
