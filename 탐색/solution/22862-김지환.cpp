79af10b4f95874b81928cb6ee46e5afe9d17d48b
jihwankim128 <jihwan1535@nate.com>
2023-07-22 03:46:39 +0900

    (가장 긴 짝수 연속한 부분수열(large)) Two Pointer

---
+++ 탐색/solution/22862-김지환.cpp
@@ -0,0 +1,54 @@
+//acmicpc 22862
+//authored by jihwankim
+//23-07-12 AM 04:22
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[1000001];
+int n, k;
+
+void input() {
+	cin >> n >> k;
+	for(int i = 0; i<n; i++) 
+		cin >> arr[i];
+}
+
+void solve() {
+	
+	int left = 0, right = 0;
+	int length = 0, cnt = 0;
+	int max_len = 0;
+	
+	for(left, right; right<n; right++) {
+		if(arr[right] % 2) {
+			//홀수일 경우
+			//홀수제거 개수가 k와 같다면 다음 홀수부터의 길이 
+			while(cnt == k) {
+				if(arr[left++]%2) cnt--;
+				else length--;
+			}
+			cnt++;
+		}
+		else {
+			//짝수일 경우 
+			length++;
+		}
+		max_len = max(max_len, length);
+	}
+	
+	cout << max_len;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
+	input();
+	solve();
+		
+	return 0;
+}
+
+//AM 04:41
