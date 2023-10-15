6852c164ca226eea98786e637bb900453c873d31
jihwankim128 <jihwan1535@nate.com>
2023-10-16 01:02:47 +0900

    (별 찍기 - 10) 다시 풀어보기

---
+++ 알고리즘기법/solution/2447-김지환.cpp
@@ -0,0 +1,23 @@
+// acmicpc 2447
+// authored by jihwankim
+// study
+// 23-08-25 AM 04:22
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+void solve(int k) {
+	cout << k;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
+	cin >> n;
+	sovle(n);
+	
+	return 0;
+}
