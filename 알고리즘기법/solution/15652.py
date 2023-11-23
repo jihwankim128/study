456c7ff4ea5ea50ea37300d093c783912e122bde
jihwan <jihwan1535@nate.com>
2023-11-23 20:02:12 +0900

    N과 M(4) - BackTracking

---
+++ 알고리즘기법/solution/15652.py
@@ -0,0 +1,22 @@
+"""
+    authored by jihwankim
+    study
+    23-11-23 PM 07:49 ~ 07:50
+"""
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [0 for i in range(n)]
+
+def solve(k, idx) :
+    if(k == m) :
+        for i in range(k):
+            print(arr[i], end = " ")
+        print()
+        return
+    for i in range(idx, n+1) :
+        arr[k] = i
+        solve(k+1, i)
+
+solve(0, 1)
